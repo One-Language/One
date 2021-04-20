be68df803a3e18f7c1db9007730716df3616e6f5
Max Base <MaxBaseCode@gmail.com>
2021-04-21 02:20:52 +0430

    Main argument: Support one-help/one-build/one-version

--- src/one.c
+++ src/one.c
@@ -17,15 +17,61 @@ void main_help(void) {
 	);
 }
 
-void main_parse(int argc, char *argv[]) {
-	printf("-->%d\n", argc);
-	for(int i=0; i<argc; i++) {
-		printf("   %s\n", argv[i]);
+void main_badcommand(int argc, char *argv[]) {
+	if(argc > 1) {
+		printf("one %s: unknown command\n", argv[1]);
+		printf("Run 'one help' for usage.");
 	}
+}
+void main_badparam(void) {
+	print("parameter flag provided but not supported!\n");
+}
+
+void main_badflag(void) {
+	print("argument flag provided but not supported!\n");
+}
+
+void main_version(int argc) {
+	if(argc > 2) {
+		main_badflag();
+	}
+	print("One version one" VERSION " " ARCH "\n");
+}
+
+void main_parse(int argc, char *argv[]) {
+	// printf("-->%d\n", argc);
+	// for(int i=0; i<argc; i++) {
+	// 	printf("   %s\n", argv[i]);
+	// }
+
+	// $ one
+	if(argc == 1) {
+		main_help();
+		return;
+	}
+
+	// $ one version
+	if(strcmp(argv[1], "version") == 0) {
+		main_version(argc);
+	}
+	// $ one build
+	else if(strcmp(argv[1], "build") == 0) {
+	}
+	// $ one run
+	else if(strcmp(argv[1], "run") == 0) {
+	}
+	// $ one help
+	else if(strcmp(argv[1], "help") == 0) {
+		main_help();
+	}
+	// otherwise
+	else {
+		main_badcommand(argc, argv);
+	}
+
 }
 
 int main(int argc, char *argv[]) {
 	main_parse(argc, argv);
-	main_help();
 	return 0;
 }
--- src/one.h
+++ src/one.h
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define printf printf
 #define print printf
+#define VERSION "0.3.0"
+#define ARCH "linux/amd64"
 
 void main_help(void);
+void main_badcommand(int argc, char *argv[]);
+void main_badparam(void);
+void main_badflag(void);
+void main_version(int argc);
 void main_parse(int argc, char *argv[]);
 int main(int argc, char *argv[]);
 
