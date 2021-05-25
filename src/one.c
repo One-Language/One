4814f2bafdc0d8d3a5b9b6babd1bfc698663a5b9
Max Base <MaxBaseCode@gmail.com>
2021-05-25 10:01:11 +0430

    Create new one.c and main func

---
+++ src/one.c
@@ -0,0 +1,14 @@
+/**
+ The One Programming Language
+
+ File: one.c
+  _        _ 
+ / \ |\ | |_    Author: Max Base
+ \_/ | \| |_    Copyright 2021
+
+ **/
+
+int main(int argc, char** argv, char** env)
+{
+
+}
