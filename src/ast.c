e095313d2971a1c4b26d7c5faf23dd9edf69ea0d
Max Base <MaxBaseCode@gmail.com>
2021-06-01 12:07:05 +0430

    Create ast.c ast.h file

---
+++ src/ast.c
@@ -0,0 +1,12 @@
+/**
+ The One Programming Language
+
+ File: ast.c
+  _        _
+ / \ |\ | |_    Max Base
+ \_/ | \| |_    Copyright 2021
+
+ **/
+
+#include "ast.h"
+
---
+++ src/ast.h
@@ -0,0 +1,16 @@
+/**
+ The One Programming Language
+
+ File: ast.h
+  _        _
+ / \ |\ | |_    Max Base
+ \_/ | \| |_    Copyright 2021
+
+ **/
+
+#ifndef _AST_H_
+#define _AST_H_
+
+
+
+#endif //_AST_H_
