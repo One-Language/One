528f25ac07810e54bf64351b43a12d44f939b3f0
Max Base <MaxBaseCode@gmail.com>
2021-06-27 06:02:29 +0430

    rewrite lexer test in another way to read test cases from a array of struct test

--- test/lexer.test.c
+++ test/lexer.test.c
@@ -10,6 +10,15 @@
 
 #include "test.h"
 
+typedef struct
+{
+	char* source;
+	Token* tokens[1024];
+} LexerTest;
+
+LexerTest lexer_tests[1024];
+unsigned int lexer_tests_count = 0;
+
 #define TEST(source)    \
 	lexer_init(source); \
 	parser_init();      \
@@ -18,28 +27,35 @@
 
 void test_lexer_log()
 {
-	for(int i=0;i<parser.tokens_count;i++) {
-		printf("[%d]\n", i);
+	for (int i = 0; i < parser.tokens_count; i++)
+	{
+		printf("[%d] %s\n", i, token_name(parser.tokens[i]->type));
 	}
-}
-
-bool test_lexer1()
-{
-	TEST("main{}");
-	test_lexer_log();
-	return false;
 }
 
 bool test_lexer()
 {
-	return test_lexer1();
+	for (unsigned i = 0; i < lexer_tests_count; i++)
+	{
+		printf("==>%d\n", i);
+	}
+	TEST("main{}");
+	test_lexer_log();
+
 	return false;
 }
 int main()
 {
 	printf("Hello to Lexer test!\n");
+
+	lexer_tests[lexer_tests_count++] = (LexerTest){"main{}", {
+																  token_make(TOKEN_VALUE_IDENTIFIER),
+																  token_make(TOKEN_OPERATOR_BRACKET_CURLY_LEFT),
+																  token_make(TOKEN_OPERATOR_BRACKET_CURLY_RIGHT),
+																  token_make(TOKEN_EOF),
+															 }};
 	bool res = test_lexer();
-	if (res == true)
+	if (lexer_tests_count == 0 || res == true)
 	{
 		printf("All tests passed.\n");
 		return 0;
