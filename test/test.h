a15f2ebc641bd7273d97653cb4da6cef62326046
John Bampton <jbampton@users.noreply.github.com>
2021-07-06 19:40:12 +1000

    Fix Spanish README (#184)
    * Fix Spanish README
    
    Use HTML5 tags
    
    * Fix case of `C`

--- test/test.h
+++ test/test.h
@@ -11,7 +11,7 @@
 #ifndef ONE_TEST_H
 #define ONE_TEST_H
 
-// You have to define a macro at CLANG or c compiler: #define _ONE_TEST_
+// You have to define a macro at CLANG or C compiler: #define _ONE_TEST_
 
 #include <stdio.h>
 #include <stdlib.h>
