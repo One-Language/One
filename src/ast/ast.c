76b41dedb6cebf023911a309e5b9e1c035926318
Max Base <MaxBaseCode@gmail.com>
2021-07-13 21:29:45 +0430

    Adding copyright header to ast.c ast.h parser.c parser.h

--- src/ast/ast.c
+++ src/ast/ast.c
@@ -1,16 +1,11 @@
 /**
  The One Programming Language
 
- File: parser/parser.h
+ File: ast/ast.c
   _        _
  / \ |\ | |_    Max Base
  \_/ | \| |_    Copyright 2021; One Language Contributors
 
  **/
 
-#ifndef _ONE_PARSER_PARSER_H_
-#define _ONE_PARSER_PARSER_H_
-
-
-
-#endif // _ONE_PARSER_PARSER_H_
+#include "ast.h"
--- src/ast/ast.h
+++ src/ast/ast.h
@@ -1,16 +1,16 @@
 /**
  The One Programming Language
 
- File: parser/parser.h
+ File: ast/ast.h
   _        _
  / \ |\ | |_    Max Base
  \_/ | \| |_    Copyright 2021; One Language Contributors
 
  **/
 
-#ifndef _ONE_PARSER_PARSER_H_
-#define _ONE_PARSER_PARSER_H_
+#ifndef _ONE_AST_AST_H_
+#define _ONE_AST_AST_H_
 
 
 
-#endif // _ONE_PARSER_PARSER_H_
+#endif // _ONE_AST_AST_H_
