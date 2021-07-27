55f336ec1f43d1eb9aeb9f50d22c407f36475041
Max Base <MaxBaseCode@gmail.com>
2021-07-28 00:28:06 +0430

    Adding parser_token functions to header file

--- src/parser/parser_token.h
+++ src/parser/parser_token.h
@@ -13,4 +13,52 @@
 
 #include "parser.h"
 
+/*
+ * @function: parser_token_skip
+ * @description: skip current tokens if these are token_is_skip we will skip from all of these and return next token.
+ * @arguments: nothing
+ * @return: Token*
+ */
+Token* parser_token_skip();
+
+/*
+ * @function: parser_token_expect
+ * @description: if type of current token is equal to `t` we will skip it and return next token, otherwise we will return current token.
+ * @arguments: TokenType t
+ * @return: Token*
+ */
+Token* parser_token_expect(TokenType t);
+
+/*
+ * @function: parser_token_next
+ * @description: go to next token and return its
+ * @arguments: nothing
+ * @return: Token*
+ */
+Token* parser_token_next();
+
+/*
+ * @function: parser_token_previous
+ * @description: go to prev token and return its
+ * @arguments: nothing
+ * @return: Token*
+ */
+Token* parser_token_previous();
+
+/*
+ * @function: parser_token_get
+ * @description: get the current token and return its
+ * @arguments: nothing
+ * @return: Token*
+ */
+Token* parser_token_get();
+
+/*
+ * @function: parser_token_at
+ * @description: get the Token* at `index`th and return its
+ * @arguments: nothing
+ * @return: Token*
+ */
+Token* parser_token_at(size_t index);
+
 #endif // _ONE_PARSER_PARSER_TOKEN_H_
