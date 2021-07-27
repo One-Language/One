8c13d5563d113f9e3d5b0cf1849301d746f6e6b9
Max Base <MaxBaseCode@gmail.com>
2021-07-27 20:51:53 +0430

    rename token_stack to parser_token name

---
+++ src/parser/parser_token.c
@@ -0,0 +1,19 @@
+/**
+ The One Programming Language
+
+ File: parser/parser_token.c
+  _        _
+ / \ |\ | |_    Max Base
+ \_/ | \| |_    Copyright 2021; One Language Contributors
+
+ **/
+
+// Global variable(s)
+extern Lexer lexer;
+
+#include "parser_token.h"
+
+void parser_token_skip()
+{
+
+}
--- src/parser/parser_token.h
+++ src/parser/parser_token.h
@@ -1,7 +1,7 @@
 /**
  The One Programming Language
 
- File: parser/token_stack.c
+ File: parser/parser_stack.h
   _        _
  / \ |\ | |_    Max Base
  \_/ | \| |_    Copyright 2021; One Language Contributors
