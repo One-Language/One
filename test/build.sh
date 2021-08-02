fa0c4007f7086d3d8b43b9ae07173773abce25d3
Max Base <MaxBaseCode@gmail.com>
2021-08-03 02:09:12 +0430

    Improve build.sh file in test

--- test/build.sh
+++ test/build.sh
@@ -1,2 +1,8 @@
-clang lexer.test.c -o lexer ../src/*.c -lLLVM-12 -D_ONE_TEST_
+clang lexer.test.c -o lexer \
+ ../src/ast/ast.c \
+ ../src/parser/parser.c \
+ ../src/parser/parser_token.c \
+ ../src/parser/parser.c \
+ ../src/lexer/lexer.c ../src/lexer/tokenizer/token.c ../src/lexer/tokenizer/tokenizer.c \
+ -lLLVM-12 -D_ONE_TEST_
 ./lexer
