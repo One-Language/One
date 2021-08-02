a5b69e8377ba1d6f2b5fed2cb6249703fb8d7208
Max Base <MaxBaseCode@gmail.com>
2021-08-03 02:23:45 +0430

    Fix build.sh in test

--- test/build.sh
+++ test/build.sh
@@ -5,7 +5,6 @@ clang lexer.test.c -o lexer \
  ../src/ast/ast.c \
  ../src/parser/parser.c \
  ../src/parser/parser_token.c \
- ../src/parser/parser.c \
  ../src/lexer/lexer.c ../src/lexer/tokenizer/token.c ../src/lexer/tokenizer/tokenizer.c \
  -lLLVM-12 -D_ONE_TEST_
 ./lexer
