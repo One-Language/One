d0976a65bc650b490000a21c63f1d0d56a5a0ea3
Max Base <MaxBaseCode@gmail.com>
2021-08-03 17:55:51 +0430

    Build other tests

--- test/build.sh
+++ test/build.sh
@@ -1,4 +1,14 @@
-# compile argument(s)
+# compile argument
+clang argument.test.c -o argument \
+ ../src/builtins/array.c \
+ ../src/builtins/error.c \
+ ../src/builtins/file.c \
+ ../src/ast/ast.c \
+ ../src/parser/parser.c \
+ ../src/parser/parser_token.c \
+ ../src/lexer/lexer.c ../src/lexer/tokenizer/token.c ../src/lexer/tokenizer/tokenizer.c \
+ -lLLVM-12 \
+ -D_ONE_TEST_
 
 # compile lexer
 clang lexer.test.c -o lexer \
@@ -13,11 +23,31 @@ clang lexer.test.c -o lexer \
  -D_ONE_TEST_
 
 # compile parser
+clang parser.test.c -o parser \
+ ../src/builtins/array.c \
+ ../src/builtins/error.c \
+ ../src/builtins/file.c \
+ ../src/ast/ast.c \
+ ../src/parser/parser.c \
+ ../src/parser/parser_token.c \
+ ../src/lexer/lexer.c ../src/lexer/tokenizer/token.c ../src/lexer/tokenizer/tokenizer.c \
+ -lLLVM-12 \
+ -D_ONE_TEST_
 
 # compiler ast
+clang ast.test.c -o ast \
+ ../src/builtins/array.c \
+ ../src/builtins/error.c \
+ ../src/builtins/file.c \
+ ../src/ast/ast.c \
+ ../src/parser/parser.c \
+ ../src/parser/parser_token.c \
+ ../src/lexer/lexer.c ../src/lexer/tokenizer/token.c ../src/lexer/tokenizer/tokenizer.c \
+ -lLLVM-12 \
+ -D_ONE_TEST_
 
 # run
-# ./arguments
+./arguments
 ./lexer
-# ./parser
-# ./ast
+./parser
+./ast
