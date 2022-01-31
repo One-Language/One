d1b2b059a1f616e823564dc40791e489cda1d994
John Bampton <jbampton@gmail.com>
2022-02-01 03:00:28 +1000

    Clean up shell scripts

--- src/parser/build.sh
+++ src/parser/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gcc ../lexer/lexer.c ../lexer/tokenizer/token.c ../lexer/tokenizer/tokenizer.c *.c ../builtins/*.c ../ast/ast.c -o parser
+gcc ../lexer/lexer.c ../lexer/tokenizer/token.c ../lexer/tokenizer/tokenizer.c ./*.c ../builtins/*.c ../ast/ast.c -o parser
