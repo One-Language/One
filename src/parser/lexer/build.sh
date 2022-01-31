9dc870a284be31aededeff07bf7c404db55e0453
John Bampton <jbampton@gmail.com>
2022-02-01 01:35:09 +1000

    Add shebangs to shell scripts
    Add one more pre-commit hook
    
    `chmod +x` shell scripts

--- src/parser/lexer/build.sh
+++ src/parser/lexer/build.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 gcc lexer.c token.c main.c  -I..\..\ ..\../builtins/*.c -D_ONE_TEST_ -o lexer
--- test/build.sh
+++ test/build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 FLAGS="-o"
 FILES="../src/builtins/array.c ../src/builtins/error.c ../src/builtins/file.c ../src/ast/ast.c ../src/parser/parser.c ../src/parser/parser_token.c ../src/lexer/lexer.c ../src/lexer/tokenizer/token.c ../src/lexer/tokenizer/tokenizer.c -lLLVM-12 -D_ONE_TEST_"
 CC="clang"
