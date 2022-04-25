645113844587a6e487134973d763cf8d35bc2b24
Restyled.io <commits@restyled.io>
2022-04-25 18:39:44 +0000

    Restyled by shellharden

--- scripts/ci/pre-commit/sort-and-uniquify-codespell-wordlist.sh
+++ scripts/ci/pre-commit/sort-and-uniquify-codespell-wordlist.sh
@@ -2,6 +2,6 @@
 
 CODESPELL_WORDLIST="codespell.txt"
 temp_file=$(mktemp)
-sort <"${CODESPELL_WORDLIST}" | uniq >"${temp_file}"
-cat "${temp_file}" >"${CODESPELL_WORDLIST}"
-rm "${temp_file}"
+sort <"$CODESPELL_WORDLIST" | uniq >"$temp_file"
+cat "$temp_file" >"$CODESPELL_WORDLIST"
+rm "$temp_file"
