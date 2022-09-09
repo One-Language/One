b00ff09af719aded797ebacfe611ddb9a1b33330
Max Base <maxbasecode@gmail.com>
2022-09-09 13:30:47 +0200

    clean code generator.c

--- src/stages/generator/generator.c
+++ src/stages/generator/generator.c
@@ -7,14 +7,17 @@
 Generator* generator_init(Parser* parser)
 {
     Generator* generator = malloc(sizeof(Generator));
+
     generator->parser = parser;
     generator->code = sdsnew("");
+
     return generator;
 }
 
 char* generator_function(Generator* generator, AstBlock* block, AstFunction* function)
 {
     sds code = sdsnew("");
+
     code = sdscatprintf(code, "function %s(", function->name);
     for (int i = 0; i < function->arguments->count; i++) {
         AstFunctionArgument* argument = function->arguments->data[i];
@@ -26,6 +29,7 @@ char* generator_function(Generator* generator, AstBlock* block, AstFunction* fun
     code = sdscatprintf(code, ") {\n");
     code = sdscatprintf(code, "%s", generator_block(generator, function->block));
     code = sdscatprintf(code, "}\n");
+
     return code;
 }
 
@@ -53,26 +57,15 @@ char* generator_block(Generator* generator, AstBlock* block)
 
 void generator_generate(Generator* generator)
 {
+    sds temp = sdsnew("");
+
+    AstBlock* block = make_block();
     AstProgram* ast = generator->parser->ast;
+
     for (int i = 0; i < ast->statements->count; i++) {
         AstStatement* stmt = ast->statements->data[i];
-        switch (stmt->type) {
-            case STATEMENT_FUNCTION:
-                generator->code = sdscatprintf(generator->code, "function %s(", stmt->stmt.function->name);
-                for (int j = 0; j < stmt->stmt.function->arguments->count; j++) {
-                    AstFunctionArgument* arg = stmt->stmt.function->arguments->data[j];
-                    generator->code = sdscatprintf(generator->code, "%s %s", arg->type, arg->name);
-                    if (j < stmt->stmt.function->arguments->count - 1) {
-                        generator->code = sdscat(generator->code, ", ");
-                    }
-                }
-                generator->code = sdscat(generator->code, ") {\n");
-                generator->code = sdscat(generator->code, generator_block(generator, stmt->stmt.function->block));
-                generator->code = sdscat(generator->code, "}\n");
-                break;
-            default:
-                break;
-        }
+        temp = sdscat(temp, generator_statement(generator, block, stmt));
     }
-//    generator->code = sdscat(generator->code, "int main() { return 0; }\n");
+
+    generator->code = temp;
 }
