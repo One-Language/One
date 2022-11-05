26c8d9883d8a9d71bae628341464ceae9fa838d1
Max Base <maxbasecode@gmail.com>
2022-11-05 19:53:53 +0330

    create array.c, array.h

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -5,14 +5,20 @@
 #include "lexer.h"
 #include "../utility/file.h"
 
+#define LEXER_CURRENT_CHAR(lexer) (lexer->input[lexer->index])
+#define LEXER_NEXT_CHAR(lexer) (lexer->input[lexer->index + 1])
+
+#define LEXER_INCREMENT_INDEX(lexer) (lexer->index++)
+#define LEXER_DECREMENT_INDEX(lexer) (lexer->index--)
+
 Lexer* lexer_new(char* file, char* data) {
     Lexer* lexer = malloc(sizeof(Lexer));
 
     lexer->file = file;
     lexer->file_dir = get_file_dir(file);
 
-    lexer->input = data != NULL ? data : file_reads(file);
-    lexer->input_size = strlen(lexer->input);
+    lexer->buffer = data != NULL ? data : file_reads(file);
+    lexer->length = strlen(lexer->buffer);
 
     lexer->tokens = malloc(sizeof(Token*));
     lexer->token_size = 0;
@@ -22,15 +28,19 @@ Lexer* lexer_new(char* file, char* data) {
 
 int lexer_run(Lexer* lexer) {
     printf("Lexer: %s\n", lexer->file);
-    printf("Input: %s\n", lexer->input);
-    printf("Input Size: %d\n", lexer->input_size);
+    printf("Input: %s\n", lexer->buffer);
+    printf("Input Size: %d\n", lexer->length);
 
-    if (lexer->input_size == 0) return 1;
+    if (lexer->length == 0) return 1;
 
-    while (lexer->input_size > 0) {
+    while (lexer->offset < lexer->length) {
         lexer_next(lexer);
     }
     return 0;
+}
+
+void lexer_next(Lexer* lexer) {
+
 }
 
 void lexer_free(Lexer* lexer) {
--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <inttypes.h>
 
 typedef struct {
 } Token;
 
 typedef struct {
-    char* input;
-    int input_size;
-
     char* file;
     char* file_dir;
+
+    char* buffer;
+    uint32_t                    offset;         // current buffer offset (in bytes)
+    // uint32_t                    position;       // current buffer position (in characters)
+    uint32_t                    length;         // buffer length (in bytes)
+    uint32_t                    lineno;         // line counter
+    uint32_t                    colno;          // column counter
 
     Token** tokens;
     int token_size;
@@ -20,5 +25,7 @@ typedef struct {
 Lexer* lexer_new(char* file, char* data);
 
 int lexer_run(Lexer* lexer);
+
+void lexer_next(Lexer* lexer);
 
 void lexer_free(Lexer* lexer);
