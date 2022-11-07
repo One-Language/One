ec58b849d398a44b1a72321b3b31372a5be719bd
Max Base <maxbasecode@gmail.com>
2022-11-07 11:20:15 +0100

    add ast_statement_type

--- src/parser/ast.h
+++ src/parser/ast.h
@@ -6,8 +6,57 @@
 #include <string.h>
 
 typedef struct {
-    
+    array* functions;
 } Ast;
+
+typedef struct {
+    char* name;
+    array* parameters;
+    array* statements;
+} AstFunction;
+
+typedef struct {
+
+} AstFunctionParameter;
+
+typedef struct {
+    array* variables;
+    array* statements;
+} AstBlock;
+
+typedef enum {
+    STATEMENT_IF,
+    STATEMENT_WHILE,
+    STATEMENT_FOR,
+    STATEMENT_RET,
+    STATEMENT_BREAK,
+    STATEMENT_CONTINUE,
+    STATEMENT_EXPRESSION
+} AstStatementType;
+
+typedef struct {
+    AstStatementType type;
+    union {
+        struct {
+            AstExpression* condition;
+            AstBlock* block;
+        } if_statement;
+        struct {
+            AstExpression* condition;
+            AstBlock* block;
+        } while_statement;
+        struct {
+            AstExpression* condition;
+            AstBlock* block;
+        } for_statement;
+        struct {
+            AstExpression* expression;
+        } ret_statement;
+        struct {
+            AstExpression* expression;
+        } expression_statement;
+    };
+} AstStatement;
 
 Ast* ast_new();
 
