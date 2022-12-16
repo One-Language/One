5484c3570d662a9ec0484d3db6d4e4f4bab11fdb
Max Base <maxbasecode@gmail.com>
2022-12-16 01:58:44 +0100

    create empty ast.c

---
+++ src/ast.c
@@ -0,0 +1,10 @@
+/**
+ The One Programming Language
+ File: ast.c
+  _        _
+ / \ |\ | |_    Max Base <maxbasecode@gmail.com>
+ \_/ | \| |_    Copyright 2023; One Language Contributors
+ **/
+
+#include "ast.h"
+
