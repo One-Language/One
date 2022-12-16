ac64bfa2c80b4f963f3532930aa934c7fb0845b2
Max Base <maxbasecode@gmail.com>
2022-12-16 11:49:37 +0100

    adding array functions to header

--- src/array.h
+++ src/array.h
@@ -18,4 +18,81 @@ typedef struct {
     void** data;
 } array_t;
 
+/**
+ * @brief Initialize array
+ * 
+ * @param void
+ * 
+ * @return array_t* 
+ */
+array_t* array_init();
+
+/**
+ * @brief Add element to array
+ * 
+ * @param array_t* array
+ * @param void* element
+ * 
+ * @return array_t* 
+ */
+array_t* array_append(array_t* array, void* element);
+
+/**
+ * @brief Remove last element from array and return it
+ * 
+ * @param array_t* array
+ * 
+ * @return void* 
+ */
+void* array_pop(array_t* array);
+
+/**
+ * @brief Get last element from array
+ * 
+ * @param array_t* array
+ * 
+ * @return void* 
+ */
+void* array_peak(array_t* array);
+
+/**
+ * @brief Get element from array
+ * 
+ * @param array_t* array
+ * @param int index
+ * 
+ * @return void* 
+ */
+void* array_get(array_t* array, int index);
+
+/**
+ * @brief Set element to array
+ * 
+ * @param array_t* array
+ * @param int index
+ * @param void* element
+ * 
+ * @return void* 
+ */
+void* array_set(array_t* array, int index, void* element);
+
+/**
+ * @brief Remove element from array
+ * 
+ * @param array_t* array
+ * @param int index
+ * 
+ * @return void* 
+ */
+void* array_remove(array_t* array, int index);
+
+/**
+ * @brief Free array
+ * 
+ * @param array_t* array
+ * 
+ * @return void 
+ */
+void array_free(array_t* array);
+
 #endif
