c66708dcd469123c0f58dbb8bf997d4629ef35b5
Max Base <maxbasecode@gmail.com>
2022-12-16 15:39:01 +0100

    adding expression structs

--- src/ast.h
+++ src/ast.h
@@ -31,8 +31,113 @@ typedef struct {
     ast_block_t* block;
 } ast_function_t;
 
+struct ast_expr_t;
+
+typedef enum {
+    AST_VALUE_BOOL,
+    AST_VALUE_INT,
+    AST_VALUE_FLOAT,
+    AST_VALUE_STRING
+} ast_value_type_t;
+
+typedef enum {
+    AST_EXPRESSION_MEMBER,
+    AST_EXPRESSION_CONDITIONAL,
+    AST_EXPRESSION_UNARY,
+    AST_EXPRESSION_BINARY,
+    AST_EXPRESSION_CALL,
+    AST_EXPRESSION_LOGICAL,
+    AST_EXPRESSION_LITERAL,
+    AST_EXPRESSION_POSTFIX,
+    AST_EXPRESSION_TERNARY,
+    AST_EXPRESSION_PREFIX,
+    AST_EXPRESSION_SUB_EXPRESSION,
+} ast_expr_type_t;
+
 typedef struct {
-    token_t* value;
+    token_type_t operator;
+    struct ast_expr_t* left;
+    struct ast_expr_t* right;
+} ast_expr_binary_t;
+
+typedef struct {
+    ast_value_type_t* type;
+    union {
+        bool boolean;
+        int integer;
+        float floating;
+        char* string;
+    } value;
+} ast_expr_literal_t;
+
+typedef struct {
+    struct ast_expr_t* callee;
+    array_t* arguments;
+} ast_expr_call_t;
+
+typedef struct {
+    token_type_t operator;
+    struct ast_expr_t* left;
+    struct ast_expr_t* right;
+} ast_expr_logical_t;
+
+typedef struct {
+    token_type_t operator;
+    struct ast_expr_t* argument;
+} ast_expr_unary_t;
+
+typedef struct {
+    struct ast_expr_t* test;
+    struct ast_expr_t* consequent;
+    struct ast_expr_t* alternate;
+} ast_expr_conditional_t;
+
+typedef struct {
+    struct ast_expr_t* condition;
+    struct ast_expr_t* true_value;
+    struct ast_expr_t* false_value;
+} ast_expr_ternary_t;
+
+typedef struct {
+    token_type_t operator;
+    struct ast_expr_t* left;
+    struct ast_expr_t* right;
+} ast_expr_assign_t;
+
+typedef struct {
+    token_type_t operator;
+    struct ast_expr_t* right;
+} ast_expr_prefix_t;
+
+typedef struct {
+    token_type_t operator;
+    struct ast_expr_t* operand;
+} ast_expr_postfix_t;
+
+typedef struct {
+    struct ast_expr_t* expression;
+} ast_expr_stmt_t;
+
+typedef struct {
+    struct ast_expr_t* expression;
+} ast_expr_sub_t;
+
+typedef struct ast_expr_t {
+    ast_expr_type_t type;
+    union {
+        ast_expr_binary_t* binary;
+        ast_expr_unary_t* unary;
+        ast_expr_call_t* call;
+        ast_expr_logical_t* logical;
+        ast_expr_literal_t* literal;
+        ast_expr_assign_t* assignment;
+        ast_expr_prefix_t* prefix;
+        ast_expr_postfix_t* postfix;
+        ast_expr_stmt_t* expression;
+        ast_expr_conditional_t* conditional;
+        ast_expr_ternary_t* ternary;
+        ast_expr_sub_t* sub_expression;
+    } expr;
 } ast_expr_t;
 
 typedef struct {
